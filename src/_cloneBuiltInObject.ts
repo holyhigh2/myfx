cb8f71821615c72878b042846148500fccc92e0f
holyhigh2 <ylem@foxmail.com>
2025-07-12 13:20:25 +0800

    v 1.11.5

--- src/_cloneBuiltInObject.ts
+++ src/_cloneBuiltInObject.ts
@@ -1,20 +1,29 @@
 import isBoolean from "./is/isBoolean"
 import isDate from "./is/isDate"
+import isMap from "./is/isMap"
+import isNumber from "./is/isNumber"
 import isRegExp from "./is/isRegExp"
+import isSet from "./is/isSet"
 import isString from "./is/isString"
 
 function cloneBuiltInObject(
   obj: unknown
-): Date | boolean | string | RegExp | null {
+): Date | Boolean | String | RegExp | Set<any> | Map<any, any> | Number | null {
   let rs = null
   if (isDate(obj)) {
     rs = new Date(obj.getTime())
   } else if (isBoolean(obj)) {
-    rs = Boolean(obj)
+    rs = new Boolean(obj.valueOf())
   } else if (isString(obj)) {
-    rs = String(obj)
+    rs = new String(obj)
   } else if (isRegExp(obj)) {
     rs = new RegExp(obj)
+  } else if (isNumber(obj)) {
+    rs = new Number(obj)
+  } else if (isSet(obj)) {
+    rs = new Set(obj)
+  } else if (isMap(obj)) {
+    rs = new Map(obj)
   }
   return rs
 }
